ff7fb064227f2ed391c2dda942664880f84174f5
mahmudamg11sm <mahmudamg11@gmail.com>
2026-02-04 13:57:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,41 +1,72 @@
 import os
 from flask import Flask, render_template, request
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, join_room, leave_room, emit
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "secret"
-socketio = SocketIO(app, cors_allowed_origins="*")
+app.config["SECRET_KEY"] = "smchat-secret"
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode="eventlet")
 
-users = {}  # sid -> username
+USERS = {}          # sid -> username
+ROOMS = {"General"} # default group
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+@socketio.on("connect")
+def connect():
+    pass
+
 @socketio.on("join")
 def on_join(data):
     username = data["username"]
-    users[request.sid] = username
-    emit("users_list", list(users.values()), broadcast=True)
+    USERS[request.sid] = username
+
+    # aika users list
+    emit("users_list", list(USERS.values()), broadcast=True)
+    emit("rooms_list", list(ROOMS), broadcast=True)
+
+@socketio.on("private_message")
+def private_message(data):
+    to = data["to"]
+    msg = data["msg"]
+    sender = USERS.get(request.sid)
+
+    for sid, user in USERS.items():
+        if user == to:
+            emit("private_message", {
+                "from": sender,
+                "msg": msg
+            }, room=sid)
+
+@socketio.on("create_room")
+def create_room(data):
+    room = data["room"]
+    ROOMS.add(room)
+    emit("rooms_list", list(ROOMS), broadcast=True)
+
+@socketio.on("join_room")
+def join_room_event(data):
+    room = data["room"]
+    join_room(room)
+    emit("room_message", {
+        "room": room,
+        "from": "System",
+        "msg": f"{USERS.get(request.sid)} ya shiga group"
+    }, room=room)
+
+@socketio.on("room_message")
+def room_message(data):
+    emit("room_message", {
+        "room": data["room"],
+        "from": USERS.get(request.sid),
+        "msg": data["msg"]
+    }, room=data["room"])
 
 @socketio.on("disconnect")
-def on_disconnect():
-    users.pop(request.sid, None)
-    emit("users_list", list(users.values()), broadcast=True)
-
-@socketio.on("dm")
-def handle_dm(data):
-    sender = data["sender"]
-    receiver = data["receiver"]
-    message = data["message"]
-
-    # tura saƙo ga receiver kaɗai
-    for sid, uname in users.items():
-        if uname == receiver:
-            emit("dm_message", {
-                "sender": sender,
-                "message": message
-            }, room=sid)
+def disconnect():
+    USERS.pop(request.sid, None)
+    emit("users_list", list(USERS.values()), broadcast=True)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
