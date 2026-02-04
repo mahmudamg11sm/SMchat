78882a93410c439f395a6ec3ad683c694445b155
mahmudamg11sm <mahmudamg11@gmail.com>
2026-02-04 13:21:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'smchat-secret'
+app.config["SECRET_KEY"] = "secret123"
 
 socketio = SocketIO(app, cors_allowed_origins="*")
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@socketio.on('message')
-def handle_message(msg):
-    send(msg, broadcast=True)
+@socketio.on("message")
+def handle_message(data):
+    send(data, broadcast=True)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
-    socketio.run(app, host='0.0.0.0', port=port)
+    socketio.run(app, host="0.0.0.0", port=port)
