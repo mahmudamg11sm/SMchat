5361da2fe99ac2a7de5ebb0ebacf37bdd23475e6
mahmudamg11sm <mahmudamg11@gmail.com>
2026-02-07 14:17:51 +0100

    Update chat.js

--- static/js/chat.js
+++ static/js/chat.js
@@ -1,103 +1,74 @@
 const socket = io();
-let myname = "";
-let current = {type:"room", name:"General"};
 
-function join(){
-  myname = document.getElementById("username").value;
-  if(!myname) return alert("Enter username");
-  socket.emit("join",{username:myname});
-  document.getElementById("login").style.display="none";
-  document.getElementById("chat").style.display="block";
-  socket.emit("join_room",{room:"General"});
+let myName = prompt("Enter username");
+let currentChannel = "General";
+
+socket.emit("join", { username: myName, room: currentChannel });
+
+const messages = document.getElementById("messages");
+
+function addMessage(html){
+  let div = document.createElement("div");
+  div.className = "message";
+  div.innerHTML = html;
+  messages.appendChild(div);
+  messages.scrollTop = messages.scrollHeight;
 }
 
-// Users list
-socket.on("users_list", users=>{
-  let u = document.getElementById("users");
-  u.innerHTML="";
-  users.forEach(x=>{
-    if(x!==myname) u.innerHTML += `<div onclick="dm('${x}')">${x}</div>`;
-  });
-});
+function sendMessage(){
+  let text = document.getElementById("msgInput").value;
+  let fileInput = document.getElementById("mediaInput");
 
-// Rooms list
-socket.on("rooms_list", rooms=>{
-  let r = document.getElementById("rooms");
-  r.innerHTML="";
-  rooms.forEach(x=> r.innerHTML += `<div onclick="joinRoom('${x}')">${x}</div>`);
-});
+  if(fileInput.files.length > 0){
+    let file = fileInput.files[0];
+    let reader = new FileReader();
 
-// DM
-function dm(u){
-  current={type:"dm",name:u};
-  document.getElementById("title").innerText="DM: "+u;
-  document.getElementById("messages").innerHTML="";
-}
-
-// Join group
-function joinRoom(r){
-  current={type:"room",name:r};
-  socket.emit("join_room",{room:r});
-  document.getElementById("title").innerText=r;
-  document.getElementById("messages").innerHTML="";
-}
-
-// Create room
-function createRoom(){
-  let r=document.getElementById("newRoom").value;
-  if(!r) return;
-  socket.emit("create_room",{room:r});
-}
-
-// Send message
-function send(){
-  let m=document.getElementById("msg").value;
-  if(!m) return;
-  if(current.type==="dm") socket.emit("private_message",{to:current.name,msg:m});
-  else socket.emit("room_message",{room:current.name,msg:m});
-  document.getElementById("msg").value="";
-}
-
-// Receive messages
-socket.on("private_message",d=> add(`${d.from}: ${d.msg}`));
-socket.on("room_message",d=> {if(d.room===current.name) add(`${d.from}: ${d.msg}`);});
-
-// Seen
-socket.on("seen",d=> add(`✓✓ Seen by ${d.to}`));
-
-function add(t){
-  let m=document.getElementById("messages");
-  let div=document.createElement("div");
-  div.innerText=t;
-  div.className="message";
-  m.appendChild(div);
-  m.scrollTop=m.scrollHeight;
-}
-
-// Media upload
-function sendMedia(){
-  let f = document.getElementById("mediaFile").files[0];
-  if(!f) return;
-  let reader = new FileReader();
-  reader.onload = function(e){
-    socket.emit("media_message",{
-      file:{
-        name:f.name,
-        data:Array.from(new Uint8Array(e.target.result))
-      },
-      room: current.name
+    reader.onload = ()=>{
+      socket.emit("channel_message",{
+        channel: currentChannel,
+        text: text,
+        media: {
+          name: file.name,
+          data: Array.from(new Uint8Array(reader.result))
+        }
+      });
+    };
+    reader.readAsArrayBuffer(file);
+    fileInput.value = "";
+  }else{
+    socket.emit("channel_message",{
+      channel: currentChannel,
+      text: text
     });
   }
-  reader.readAsArrayBuffer(f);
+
+  document.getElementById("msgInput").value = "";
 }
 
-// Receive media
-socket.on("media_message",d=>{
-  let m=document.getElementById("messages");
-  let div=document.createElement("div");
-  div.className="message";
-  if(d.type==="image") div.innerHTML = `<b>${d.from}</b><br><img src="/${d.url}" style="max-width:100%;border-radius:10px">`;
-  if(d.type==="video") div.innerHTML = `<b>${d.from}</b><br><video src="/${d.url}" controls style="max-width:100%;border-radius:10px"></video>`;
-  m.appendChild(div);
-  m.scrollTop=m.scrollHeight;
+socket.on("new_channel_message", d=>{
+  if(d.type === "image"){
+    addMessage(`<b>${d.sender}</b><br><img src="/${d.media_url}">`);
+  }else if(d.type === "video"){
+    addMessage(`<b>${d.sender}</b><br><video controls src="/${d.media_url}"></video>`);
+  }else{
+    addMessage(`<b>${d.sender}</b>: ${d.text}`);
+  }
+});
+
+socket.on("system", msg=>{
+  addMessage(`<i>${msg}</i>`);
+});
+
+socket.on("users_list", users=>{
+  let u = document.getElementById("users");
+  u.innerHTML = "";
+  users.forEach(x=>{
+    let d = document.createElement("div");
+    d.innerText = x;
+    d.onclick = ()=>{
+      openView("chatView");
+      document.getElementById("chatTitle").innerText = x;
+    };
+    u.appendChild(d);
+  });
 });
