13ff197b15ecbbec663f4df69a3b95be18dbf4fc
mahmudamg11sm <mahmudamg11@gmail.com>
2026-02-07 14:39:23 +0100

    Update chat.js

--- static/js/chat.js
+++ static/js/chat.js
@@ -1,74 +1,33 @@
 const socket = io();
-
-let myName = prompt("Enter username");
-let currentChannel = "General";
-
-socket.emit("join", { username: myName, room: currentChannel });
-
-const messages = document.getElementById("messages");
-
-function addMessage(html){
-  let div = document.createElement("div");
-  div.className = "message";
-  div.innerHTML = html;
-  messages.appendChild(div);
-  messages.scrollTop = messages.scrollHeight;
-}
+let myname = "{{ username }}";
 
 function sendMessage(){
-  let text = document.getElementById("msgInput").value;
-  let fileInput = document.getElementById("mediaInput");
-
-  if(fileInput.files.length > 0){
-    let file = fileInput.files[0];
-    let reader = new FileReader();
-
-    reader.onload = ()=>{
-      socket.emit("channel_message",{
-        channel: currentChannel,
-        text: text,
-        media: {
-          name: file.name,
-          data: Array.from(new Uint8Array(reader.result))
-        }
-      });
-    };
-    reader.readAsArrayBuffer(file);
-    fileInput.value = "";
-  }else{
-    socket.emit("channel_message",{
-      channel: currentChannel,
-      text: text
-    });
-  }
-
-  document.getElementById("msgInput").value = "";
+  const msg = document.getElementById("msg").value;
+  socket.emit("channel_message", {channel:"General", text:msg});
+  document.getElementById("msg").value="";
 }
 
 socket.on("new_channel_message", d=>{
-  if(d.type === "image"){
-    addMessage(`<b>${d.sender}</b><br><img src="/${d.media_url}">`);
-  }else if(d.type === "video"){
-    addMessage(`<b>${d.sender}</b><br><video controls src="/${d.media_url}"></video>`);
-  }else{
-    addMessage(`<b>${d.sender}</b>: ${d.text}`);
+  const m = document.getElementById("messages");
+  let div = document.createElement("div");
+  div.className = "message";
+  let badges = "";
+  if(d.sender==="admin"){ badges += '<span class="badge admin">ADMIN</span>'; }
+  if(d.sender==="verified"){ badges += '<span class="badge verified">✔</span>'; }
+  if(d.type==="text"){
+    div.innerHTML = `<b>${d.sender}</b>${badges}: ${d.text}`;
   }
+  if(d.type==="image"){
+    div.innerHTML = `<b>${d.sender}</b>${badges}: <img src="/${d.media_url}" class="media">`;
+  }
+  if(d.type==="video"){
+    div.innerHTML = `<b>${d.sender}</b>${badges}: <video src="/${d.media_url}" controls class="media"></video>`;
+  }
+  m.appendChild(div);
+  m.scrollTop = m.scrollHeight;
 });
 
-socket.on("system", msg=>{
-  addMessage(`<i>${msg}</i>`);
-});
-
-socket.on("users_list", users=>{
-  let u = document.getElementById("users");
-  u.innerHTML = "";
-  users.forEach(x=>{
-    let d = document.createElement("div");
-    d.innerText = x;
-    d.onclick = ()=>{
-      openView("chatView");
-      document.getElementById("chatTitle").innerText = x;
-    };
-    u.appendChild(d);
-  });
-});
+function switchView(v){
+  document.querySelectorAll(".view").forEach(x=>x.classList.remove("active"));
+  document.getElementById("view-"+v).classList.add("active");
+}
