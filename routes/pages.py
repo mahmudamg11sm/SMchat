409ecc1ab775f23a799b805fec9a92b770d0036b
mahmudamg11sm <mahmudamg11@gmail.com>
2026-02-10 20:13:18 +0100

    Update pages.py

--- routes/pages.py
+++ routes/pages.py
@@ -1,13 +1,43 @@
 from flask import Blueprint, render_template, request
-from flask_login import login_required
+from flask_login import login_required, current_user
 from models.user import User
+from models.post import Post
+from app import db
 
 pages_bp = Blueprint("pages", __name__)
+
 
 @pages_bp.route("/")
 @login_required
 def home():
-    return render_template("pages/home.html")
+    users = User.query.filter(User.id != current_user.id).all()
+    unread = Post.query.filter_by(read=False).all()
+    return render_template("pages/home.html", users=users, unread=unread)
+
+
+@pages_bp.route("/chat/<username>", methods=["GET", "POST"])
+@login_required
+def chat(username):
+    other = User.query.filter_by(username=username).first_or_404()
+
+    if request.method == "POST":
+        text = request.form.get("text")
+        if text:
+            msg = Post(
+                author_id=current_user.id,
+                content=text,
+                is_public=False
+            )
+            db.session.add(msg)
+            db.session.commit()
+
+    messages = Post.query.order_by(Post.created_at.asc()).all()
+    return render_template(
+        "pages/chat.html",
+        other=other,
+        messages=messages
+    )
+
 
 @pages_bp.route("/search")
 @login_required
