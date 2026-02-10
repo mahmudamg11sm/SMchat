468733c1d5d19208b1ad3c4435f545287e5ed361
mahmudamg11sm <mahmudamg11@gmail.com>
2026-02-10 20:07:22 +0100

    Update tabs.js

--- static/js/tabs.js
+++ static/js/tabs.js
@@ -1,1 +1,14 @@
+document.querySelectorAll(".tabs a").forEach(tab => {
+  tab.addEventListener("click", e => {
+    e.preventDefault();
+    const target = tab.dataset.tab;
 
+    document.querySelectorAll(".tab-view").forEach(v => {
+      v.style.display = "none";
+    });
+
+    if (target) {
+      document.getElementById(target).style.display = "block";
+    }
+  });
+});
