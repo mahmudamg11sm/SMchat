bbe17119fa4726caee9ecbe591a1117d1c166737
mahmudamg11sm <mahmudamg11@gmail.com>
2026-02-10 20:55:52 +0100

    Update pages.py

--- routes/pages.py
+++ routes/pages.py
@@ -1,49 +1,34 @@
-from flask import Blueprint, render_template, request
-from flask_login import login_required, current_user
-from models.user import User
-from models.post import Post
-from app import db
+from flask import Blueprint, render_template
+from flask_login import login_required
 
 pages_bp = Blueprint("pages", __name__)
 
-
-@pages_bp.route("/")
+@pages_bp.route("/home")
 @login_required
 def home():
-    users = User.query.filter(User.id != current_user.id).all()
-    unread = Post.query.filter_by(read=False).all()
-    return render_template("pages/home.html", users=users, unread=unread)
+    return render_template("pages/home.html")
 
-
-@pages_bp.route("/chat/<username>", methods=["GET", "POST"])
+@pages_bp.route("/public")
 @login_required
-def chat(username):
-    other = User.query.filter_by(username=username).first_or_404()
+def public():
+    return render_template("pages/public.html")
 
-    if request.method == "POST":
-        text = request.form.get("text")
-        if text:
-            msg = Post(
-                author_id=current_user.id,
-                content=text,
-                is_public=False
-            )
-            db.session.add(msg)
-            db.session.commit()
-
-    messages = Post.query.order_by(Post.created_at.asc()).all()
-    return render_template(
-        "pages/chat.html",
-        other=other,
-        messages=messages
-    )
-
+@pages_bp.route("/notifications")
+@login_required
+def notifications():
+    return render_template("pages/notifications.html")
 
 @pages_bp.route("/search")
 @login_required
 def search():
-    q = request.args.get("q")
-    result = None
-    if q:
-        result = User.query.filter_by(username=q).first()
-    return render_template("pages/search.html", result=result)
+    return render_template("pages/search.html")
+
+@pages_bp.route("/users")
+@login_required
+def users():
+    return render_template("pages/users.html")
+
+@pages_bp.route("/info")
+@login_required
+def info():
+    return render_template("pages/info.html")
