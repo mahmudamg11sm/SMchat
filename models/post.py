eccdee225ed7184912f916c2cbc9433d3d8f87ca
mahmudamg11sm <mahmudamg11@gmail.com>
2026-02-12 15:54:30 +0100

    Update post.py

--- models/post.py
+++ models/post.py
@@ -1,10 +1,28 @@
-from app import db
-from datetime import datetime
+from flask import g
 
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    author_id = db.Column(db.Integer, nullable=False)
-    content = db.Column(db.Text, nullable=False)
-    is_public = db.Column(db.Boolean, default=False)
-    read = db.Column(db.Boolean, default=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+class Post:
+    TABLE = "posts"
+
+    def __init__(self, user, content, id=None):
+        self.user = user
+        self.content = content
+        self.id = id
+
+    @staticmethod
+    def get_db():
+        return g.db
+
+    @classmethod
+    def create(cls, user, content):
+        db = cls.get_db()
+        c = db.cursor()
+        c.execute(f"INSERT INTO {cls.TABLE}(user,content) VALUES(?,?)", (user, content))
+        db.commit()
+        return cls(user, content, c.lastrowid)
+
+    @classmethod
+    def all(cls):
+        db = cls.get_db()
+        c = db.cursor()
+        c.execute(f"SELECT * FROM {cls.TABLE} ORDER BY id DESC")
+        return [cls(row["user"], row["content"], row["id"]) for row in c.fetchall()]
