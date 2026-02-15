26b7cc8604eb33262a40f6fe9d261ab427e5de9a
mahmudamg11sm <mahmudamg11@gmail.com>
2026-02-15 11:19:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -232,5 +232,5 @@ def reaction(data):
 
 # ------------------ MAIN ------------------
 if __name__=="__main__":
-    port=int(os.environ.get("PORT",10000))
+    port=int(os.environ.get("PORT",5000))
     socketio.run(app, host="0.0.0.0", port=port)
